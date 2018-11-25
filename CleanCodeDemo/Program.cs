586567b04fdf8e1c7f56bfaa63539f5891bf160a
okantiryaki <okantiryaki@outlook.com>
2018-11-26 00:15:14 +0300

    26.11.2018 - 00:15 CleanCodeDemo

--- CleanCodeDemo/Program.cs
+++ CleanCodeDemo/Program.cs
@@ -10,12 +10,123 @@ namespace CleanCodeDemo
     {
         static void Main(string[] args)
         {
-            // The code provided will print ‘Hello World’ to the console.
-            // Press Ctrl+F5 (or go to Debug > Start Without Debugging) to run your app.
-            Console.WriteLine("Hello World!");
-            Console.ReadKey();
+            IProductService productService = new ProductManager(new IsBankAdapter());
 
-            // Go to http://aka.ms/dotnet-get-started-console to continue learning how to build a console app! 
+            productService.Sell(new Product { ProductId = 1, ProductName = "Laptop", Price = 1000 },
+                new Student { Id = 1, Name = "Okan", DiscountRate = 0.90m });
+        }
+    }
+
+    class CustomerBase : IEntity
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+
+    class Customer : CustomerBase
+    {
+        public string CustomerType { get; set; }
+        public decimal DiscountRate { get; set; }
+    }
+
+    class Student : Customer
+    {
+        
+    }
+
+    class Officer : Customer
+    {
+
+    }
+
+    class Product : IEntity
+    {
+        public int ProductId { get; set; }
+
+        public string ProductName { get; set; }
+
+        public decimal Price { get; set; }
+    }
+
+    class ProductManager : IProductService
+    {
+        private IBankService _bankService;
+
+        public ProductManager(IBankService bankService)
+        {
+            _bankService = bankService;
+        }
+
+        public void Sell(Product product, Customer stundentCustomer)
+        {
+            decimal price = product.Price;
+
+            price = product.Price * (decimal)stundentCustomer.DiscountRate;
+
+            price = _bankService.ConvertRate(new CurrencyRate { Currency = 1, Price = price });
+
+            Console.WriteLine(price);
+            Console.ReadLine();
+        }
+
+    }
+
+    internal interface IProductService
+    {
+        void Sell(Product product, Customer customer);
+
+    }
+
+    class CentralBankAdapter : IBankService
+    {
+        public decimal ConvertRate(CurrencyRate currencyRate)
+        {
+            CentralBankSerice centralBankSerice = new CentralBankSerice();
+            return centralBankSerice.ConvertCuurency(currencyRate);
+        }
+    }
+
+    class IsBankAdapter : IBankService
+    {
+        public decimal ConvertRate(CurrencyRate currencyRate)
+        {
+            IsBankSerice ısBankSerice = new IsBankSerice();
+            return ısBankSerice.ConvertCuurency(currencyRate);
+        }
+    }
+
+    class FakeBankService : IBankService
+    {
+        public decimal ConvertRate(CurrencyRate currencyRate)
+        {
+            return currencyRate.Price / 5.30m;
+        }
+    }
+
+    internal interface IBankService
+    {
+        decimal ConvertRate(CurrencyRate currencyRate);
+    }
+
+    class CurrencyRate
+    {
+        public decimal Price { get; set; }
+        public int Currency { get; set; }
+    }
+
+    class CentralBankSerice
+    {
+        public decimal ConvertCuurency(CurrencyRate currencyRate)
+        {
+            return currencyRate.Price / (decimal)5.28;
+        }
+    }
+
+    class IsBankSerice
+    {
+        public decimal ConvertCuurency(CurrencyRate currencyRate)
+        {
+            return currencyRate.Price / (decimal)1;
         }
     }
 }
